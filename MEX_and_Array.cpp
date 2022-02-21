14b51ad4ccfcf880955ac6ff709126358bfe0094
Subhendu Karmakar <subhendukarmakar.pixel@gmail.com>
2022-02-21 17:16:51 +0530

    new commit

---
+++ MEX_and_Array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	
+}
+
+int main() {
+	int t=1;
+	scanf("%d", &t);
+	for(int i=1; i<=t; ++i) {
+		// printf("Case #%d: ", i);
+		solve();
+	}
+	return 0;
+}
